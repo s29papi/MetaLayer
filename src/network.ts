9abc5a33f381496ef705fd988748618703786c2c
S29 <nots29vv@gmail.com>
2025-11-04 16:33:01 +0100

    mainnet configuration

--- src/network.ts
+++ src/network.ts
@@ -17,6 +17,14 @@ export const NETWORKS: Record<string, NetworkConfig> = {
     explorerUrl: 'https://chainscan-galileo.0g.ai/',
     chainId: 16602,
     metaLayerAddress: `0x${"3Ca8B5e3fCD85c9960C3F8c09f0cbe6145ccdC27"}`
-  }
+  },
+  mainnet: {
+    name: 'mainnet',
+    rpcUrl: 'https://evmrpc.0g.ai/',       // <- change if you have a different RPC
+    indexerUrl: 'https://indexer-storage-turbo.0g.ai/', // <- change to the real indexer endpoint
+    explorerUrl: 'https://chainscan.0g.ai/',
+    chainId: 16661,
+    metaLayerAddress: `0x${"0xd15f8fc862d29b19d8f614c4aceb727f67986677"}`
+  },
 };
    
